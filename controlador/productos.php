9422edda5de6f7f4ab11b5f8c5e04e8f3d6bdf3a
johnnycea <ceaceajohnny@gmail.com>
2023-03-29 15:28:57 -0300

    Eliminar y modificar
    Clases Producto y usuario

--- controlador/productos.php
+++ controlador/productos.php
@@ -4,8 +4,8 @@ $Producto = new Producto();
 
 Flight::route('GET /productos',array($Producto,'obtenerProductos'));
 Flight::route('POST /productos',array($Producto,'crearProducto'));
-//Flight::route('PUT /productos',array($Producto,'modificarProducto'));
-
+Flight::route('PUT /productos',array($Producto,'modificarProducto'));
+Flight::route('DELETE /productos',array($Producto,'eliminarProducto'));
 
 
 
--- controlador/usuarios.php
+++ controlador/usuarios.php
@@ -6,7 +6,9 @@ Flight::route('GET /usuario',array($Usuario,'obtenerUsuario'));
 
 Flight::route('POST /usuario',array($Usuario,'crearUsuario'));
 
-//Flight::route('PUT /usuario',array($Usuario,'modificarUsuario'));
+Flight::route('PUT /usuario',array($Usuario,'modificarUsuario'));
+
+Flight::route('DELETE /usuario',array($Usuario,'eliminarUsuario'));
 
 
 ?>
