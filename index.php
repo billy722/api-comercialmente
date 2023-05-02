28759b406e31683e933175d88ea5634b3ddf5ec3
Billy Salazar <billysalazar@MBP-de-Billy.lan>
2023-05-02 16:50:04 -0400

    normalidad proyecto

--- index.php
+++ index.php
@@ -1,47 +1,38 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Credentials: true');
+header("Access-Control-Allow-Methods: DELETE, GET, POST, OPTIONS, PUT");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Method, Access-Control-Allow-Origin, Origin, Authorization");
+
 require 'flight/Flight.php';
-require 'modelo/Cliente.php';
+require 'clases/Respuestas.php';
 
-$Cliente = new Cliente();
-Flight::route('GET /clientes',array($Cliente,'obtenerClientes'));
-Flight::json("hola");
-echo "hola";
-Flight::start();
+//IMPORTACION DE MODELO (CLASES)
+require './modelo/Conexion.php';
 
-// header('Access-Control-Allow-Origin: *');
-// header('Content-Type: application/json');
-// header('Access-Control-Allow-Credentials: true');
-// header("Access-Control-Allow-Methods: DELETE, GET, POST, OPTIONS, PUT");
-// header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Method, Access-Control-Allow-Origin, Origin, Authorization");
-
-// require 'flight/Flight.php';
-// require 'clases/Respuestas.php';
-
-// //IMPORTACION DE MODELO (CLASES)
-// require './modelo/Conexion.php';
-
-// require './modelo/Producto.php';
-// require './modelo/Cliente.php';
-// require './modelo/Usuario.php';
-// require './modelo/Cotizacion.php';
-// require './modelo/Colaborador.php';
-// require './modelo/Problematica.php';
-// require './modelo/Cuestionario.php';
-// require './modelo/Pregunta.php';
+require './modelo/Producto.php';
+require './modelo/Cliente.php';
+require './modelo/Usuario.php';
+require './modelo/Cotizacion.php';
+require './modelo/Colaborador.php';
+require './modelo/Problematica.php';
+require './modelo/Cuestionario.php';
+require './modelo/Pregunta.php';
+require './modelo/Alternativa.php';
 // require './modelo/Alternativa.php';
-// // require './modelo/Alternativa.php';
 
-// //IMPORTAR CONTROLADORES
-// require 'controlador/productos.php';
-// require 'controlador/clientes.php';
-// require 'controlador/usuarios.php';
-// require 'controlador/cotizaciones.php';
-// require 'controlador/colaboradores.php';
-// require 'controlador/problematicas.php';
-// require 'controlador/cuestionarios.php';
-// require 'controlador/preguntas.php';
+//IMPORTAR CONTROLADORES
+require 'controlador/productos.php';
+require 'controlador/clientes.php';
+require 'controlador/usuarios.php';
+require 'controlador/cotizaciones.php';
+require 'controlador/colaboradores.php';
+require 'controlador/problematicas.php';
+require 'controlador/cuestionarios.php';
+require 'controlador/preguntas.php';
+require 'controlador/alternativas.php';
 // require 'controlador/alternativas.php';
-// // require 'controlador/alternativas.php';
 
-// Flight::start();
+Flight::start();
 ?>
